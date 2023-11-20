05b0f51ecac2866b6f6c6149bce8dda7323c92ce
Natalia GF <natimetal83@gmail.com>
2023-11-20 23:08:48 +0100

    propiedades

--- app.js
+++ app.js
@@ -26,4 +26,51 @@ const jsonData = {
 
   const titles = jsonData.results.map(todo => todo.title);
  console.log(titles);
+
+ const jsonData = {
+    "results": [
+      {
+        "title": "Estudiar Js",
+        "priority": "alta",
+        "isDone": false
+      },
+      {
+        "title": "Estudiar CSS",
+        "priority": "alta",
+        "isDone": true
+      },
+      {
+        "title": "Estudiar OOP",
+        "priority": "media",
+        "isDone": false
+      },
+      {
+        "title": "Estudiar IA",
+        "priority": "baja",
+        "isDone": false
+      }
+    ]
+  };
+
+  const tableContainer = document.getElementById("table-container");
+  
+  const table = document.createElement("table");
+  
+  const headerRow = table.insertRow(0);
+  Object.keys(jsonData.results[0]).forEach(key => {
+    const headerCell = document.createElement("th");
+    headerCell.textContent = key;
+    headerRow.appendChild(headerCell);
+  });
+  
+  jsonData.results.forEach((todo, index) => {
+    const row = table.insertRow(index + 1);
+  
+    Object.values(todo).forEach(value => {
+      const cell = row.insertCell();
+      cell.textContent = value;
+    });
+  });
+  tableContainer.appendChild(table);
+  
   
